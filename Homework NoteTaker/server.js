29d9f0b0fb928d6b44488ff108e67531ad1573ef
Austin Cochran <cochran7004@gmail.com>
2021-01-19 21:53:24 -0500

    moved folder

---
+++ Homework NoteTaker/server.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('./routes/routes')(app);
+// a
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
